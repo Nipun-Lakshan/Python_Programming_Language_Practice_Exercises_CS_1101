2b1b3e719f0d1c132ea9a366b4db38fb29929a24
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-18 22:37:31 +0530

    Practice 03

--- Practice03.py
+++ Practice03.py
@@ -1,1 +1,9 @@
-# CS 1101 - Week 09 - Video 05 - Variables
+# CS 1101 - Week 09 - Video 05 - Receiving Input
+
+# Code 01
+input(
+    "\nWhat is your name? ")  # Print String and Inserted the Value to the System
+
+# Code 02
+name = input("What is your name? ")
+print("Hello " + name)
---
+++ Practice04.py
@@ -0,0 +1,2 @@
+# CS 1101 - Week 09 - Video 06 - Type Conversion
+
