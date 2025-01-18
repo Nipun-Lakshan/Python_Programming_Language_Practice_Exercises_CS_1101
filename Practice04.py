e56555770bdca4e533525253fa911f06c517c6db
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-18 23:59:01 +0530

    Practice 04

--- Practice04.py
+++ Practice04.py
@@ -1,2 +1,12 @@
 # CS 1101 - Week 09 - Video 06 - Type Conversion
 
+# Code 01 : Example
+birth_year = int(input("\nEnter your birth year : "))
+age = 2025 - birth_year
+print(age)
+
+# Code 02 : Exercise
+num1 = float(input("\nFirst  Number : "))
+num2 = float(input("Second Number : "))
+sum = (num1 + num2)
+print("Sum           : " + str(sum))
