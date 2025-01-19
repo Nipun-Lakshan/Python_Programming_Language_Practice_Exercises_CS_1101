d6a2b17da8797d7d9eaed3bbbcd1577c93021f77
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-20 02:27:12 +0530

    Practice 10

---
+++ Practice10.py
@@ -0,0 +1,13 @@
+# CS 1101 - Week 09 - Video 12 - If Statements
+
+temperature = 35
+if (temperature > 30):
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif (temperature > 20):
+    print("It's a nice day!")
+elif (temperature > 10):
+    print("It's a bit cold day")
+else:
+    print("It's cold")
+print("Done")
