401753407c329284e9f9d93a2deaf25733c22701
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-20 01:35:11 +0530

    Practice 06

---
+++ Practice06.py
@@ -0,0 +1,9 @@
+# CS 1101 - Week 09 - Video 08 - Arithmetic Operators
+
+print(10 + 3)  # 13
+print(10 - 3)  # 07
+print(10 * 3)  # 30
+print(10 / 3)  # 3.3333333333333335
+print(10 // 3)  # 3
+print(10 % 3)  # 1
+print(10**3)  # 1000
