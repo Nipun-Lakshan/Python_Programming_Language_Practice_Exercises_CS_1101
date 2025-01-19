d88577e27984c3acf5e4cf5b698889e3884b04a5
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-20 02:42:28 +0530

    Practice 11

---
+++ Practice11.py
@@ -0,0 +1,20 @@
+# CS 1101 - Week 09 - Video 13 - While Loops
+
+# Code 01
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+
+# Code 02
+i = 1
+while (i <= 5):
+    print(i)
+    i = i + 1
+
+# Code 03
+i = 1
+while (i <= 10):
+    print(i * '*')
+    i = i + 1
