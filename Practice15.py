fef8d5d81bc7dde001b678ad421345b74fb1b5b2
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-20 03:57:22 +0530

    Practice 15

---
+++ Practice15.py
@@ -0,0 +1,19 @@
+# CS 1101 - Week 09 - Video 17 - The range() Function
+
+numbers = range(5)
+print(numbers)  # Print the range object
+
+for number in numbers:
+    print(number)
+
+numbers = range(5, 10)
+for number in numbers:
+    print(number)
+
+numbers = range(5, 10, 2)
+
+for number in numbers:
+    print(number)
+
+for number in range(5):
+    print(number)
