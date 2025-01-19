69ce8ed7354e814e9c56c4f284c0243dd08feacb
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-20 01:59:34 +0530

    Practice 08

---
+++ Practice08.py
@@ -0,0 +1,19 @@
+# CS 1101 - Week 09 - Video 10 - Comparison Operators
+
+x = (3 > 2)
+print(x)  # True
+
+x = (3 >= 2)
+print(x)  # True
+
+x = (3 < 2)
+print(x)  # False
+
+x = (3 <= 2)
+print(x)  # False
+
+x = (3 == 2)
+print(x)  # False
+
+x = (3 != 2)
+print(x)  # True
