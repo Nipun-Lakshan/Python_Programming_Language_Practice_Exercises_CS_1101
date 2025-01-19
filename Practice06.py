4ff56de4f8d604b0c3e4995fe71ff6e9b9a888e0
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-20 01:50:08 +0530

    Practice 07

--- Practice06.py
+++ Practice06.py
@@ -1,5 +1,6 @@
 # CS 1101 - Week 09 - Video 08 - Arithmetic Operators
 
+# Code 01
 print(10 + 3)  # 13
 print(10 - 3)  # 07
 print(10 * 3)  # 30
@@ -7,3 +8,21 @@ print(10 / 3)  # 3.3333333333333335
 print(10 // 3)  # 3
 print(10 % 3)  # 1
 print(10**3)  # 1000
+
+# Code 02
+x = 10
+print(x)  # 10
+x = x + 3
+print(x)  # 13
+x += 3
+print(x)  # 16
+x -= 3
+print(x)  # 13
+x *= 3
+print(x)  # 39
+x /= 3
+print(x)  # 13.0
+x //= 3
+print(x)  # 4
+x **= 3
+print(x)  # 64
---
+++ Practice07.py
@@ -0,0 +1,16 @@
+# CS 1101 - Week 09 - Video 09 - Operator Precedence
+
+x = 10 + 3 * 2
+print(x)  # 16
+x = (10 + 3) * 2
+print(x)  # 26
+'''
+
+Order Precedence
+================
+
+01. Parenthesis
+02. Division & Multiplication
+03. Addition & Subtraction
+
+'''
