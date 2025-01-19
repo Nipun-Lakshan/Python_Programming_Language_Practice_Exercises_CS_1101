97844d80caa1c42aa3fce3154ce59ff02cae7739
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-20 02:13:31 +0530

    Practice 09

---
+++ Practice09.py
@@ -0,0 +1,8 @@
+# CS 1101 - Week 09 - Video 11 - Logical Operators
+
+price = 25
+print((price > 10))  # True
+print((price > 10) and (price < 30))  # True
+price = 5
+print((price > 10) or (price < 30))  # True
+print(not (price > 10))  # True
