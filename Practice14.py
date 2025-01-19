5ab8632dccf4af9e5993f0bfdf039d862676f09f
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-20 03:28:14 +0530

    Practice 14

---
+++ Practice14.py
@@ -0,0 +1,19 @@
+# CS 1101 - Week 09 - Video 16 - For Loops
+
+# Code 01
+numbers = [1, 2, 3, 4, 5]
+print(numbers)  # [1, 2, 3, 4, 5]
+
+# Code 02
+for item in numbers:
+    print(item)
+
+# Code 03
+i = 1
+while i < len(numbers):
+    print(i)
+    i = i + 1
+
+# Code 04
+for i in range(0, 5):
+    print(i)
