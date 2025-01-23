2bc0ba3491170f57243524063ef1ca5b266def11
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-23 09:34:49 +0530

    Practice 23

--- Practice22.py
+++ Practice22.py
@@ -1,4 +1,6 @@
 # CS 1101 - Week 09 - Exercise 04
+print()
 length = int(input("Enter the Length : "))
-for i in range(0, (length)):
+print()
+for i in range(0, length):
     print("*" * length)
---
+++ Practice23.py
@@ -0,0 +1,8 @@
+# CS 1101 - Week 09 - Exercise 05
+print()
+length = int(input("Enter the Length : "))
+print()
+for i in range(0, (length)):
+    for j in range(0, length):
+        print("*", end="")
+    print()
