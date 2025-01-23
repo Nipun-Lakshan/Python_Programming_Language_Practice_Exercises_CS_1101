ec575d5310da1ee8fbd77fce1c4de22f5f4de2ee
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-23 08:52:34 +0530

    Practice 20

---
+++ Practice20.py
@@ -0,0 +1,4 @@
+# CS 1101 - Week 09 - Exercise 02
+temperature = float(input("Enter Temperature in Celsius : "))
+fahrenheit = (((temperature * 9) / 5) + 32)
+print(f"{temperature} {chr(176)}C = {fahrenheit} {chr(176)}F")
