7e950a4377b2c7d617b34160d62f684b2f0beb8a
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-23 10:15:10 +0530

    Practice 26

--- Practice26.py
+++ Practice26.py
@@ -1,2 +1,9 @@
 # CS 1101 - Week 09 - Exercise 08
-
+print()
+height = int(input("Enter the Height : "))
+print()
+for i in range(0, height):
+    for j in range(0, (height - 1)):
+        print(" ", end="")
+    print("*" * (2 * (i + 1) - 1))
+    height = height - 1
