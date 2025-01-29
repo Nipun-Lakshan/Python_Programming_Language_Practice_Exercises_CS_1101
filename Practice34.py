09e6ec018320acf588e3add0b05d06b955ba25bd
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-29 16:09:14 +0530

    Practice 34

---
+++ Practice34.py
@@ -0,0 +1,53 @@
+# CS 1101 - Week 10 - Practice 05 - Type Conversion
+
+x = '145'
+print(type(x))
+print(x)
+print()
+
+y = int(x)
+print(type(y))
+print(y)
+print()
+
+z = int(x, 8)
+print(type(z))
+print(z)
+print()
+
+print(str(42))
+
+m = float(120)
+print(type(m))
+print(m)
+print()
+
+a = str(688)
+print(type(a))
+print(a)
+print()
+
+z = hex(688)
+print(type(z))
+print(z)
+print()
+
+o = complex(5, 3.1)
+print(type(o))
+print(o)
+print()
+
+t = tuple('random')
+print(type(t))
+print(t)
+print()
+
+l = list('random')
+print(type(l))
+print(l)
+print()
+
+s = set('random')
+print(type(s))
+print(s)
+print()
