341e026e54f96eb15b148b27dfd0a0cce8a62a44
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-02-01 23:05:08 +0530

    Practice 40

---
+++ Practice40.py
@@ -0,0 +1,15 @@
+# CS 1101 - Week 11 Videos - NumPy - Practice 01
+
+import numpy as np
+
+array_a = np.array([1, 2, 3, 4, 5])
+print(array_a)
+print([1, 2, 3, 4, 5])
+
+array_a = np.array([1, 2, 3, 4, 5, "Derrick"])
+print(array_a)
+print([1, 2, 3, 4, 5, "Derrick"])
+
+array_b = np.array([[1, 2, 3, 4, 5], [1,2,3,4,5]])
+print(array_b)
+print(array_b.shape)
