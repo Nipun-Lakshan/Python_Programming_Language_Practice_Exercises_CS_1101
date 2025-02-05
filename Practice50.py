775af88691e79ade5fa9dbcf21c540da5edd5caf
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-02-05 10:23:09 +0530

    Practice 50

---
+++ Practice50.py
@@ -0,0 +1,26 @@
+# CS 1101 - Week 11 Videos - Matplotlib - Practice 03
+
+import matplotlib.pyplot as plt
+'''
+Intro to Graph
+==============
+
+The number of views a youtube channel got in the last 7 days.
+'''
+
+views = [534, 689, 258, 401, 724, 689, 350]
+days = range(1, 8)
+'''
+Plot Syntax Description
+=======================
+
+plot(x, y, label = '<name_of_the_graph>) // plot a graph with a legend.
+'''
+
+plt.plot(days, views, label='Youtube Views', color='r', marker='o', markerfacecolor='g')  
+plt.xlabel('Day No.')
+plt.ylabel('Views')
+plt.legend(loc='lower right'
+           )  # Legend with changing the default location of upper left.
+plt.title('Youtube Views on a Daily Basis')
+plt.show()
