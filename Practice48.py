fe124fe9eacd6d8e7b5202debab5518d6c463c14
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-02-05 07:48:25 +0530

    Practice 48

--- Practice48.py
+++ Practice48.py
@@ -1,1 +1,16 @@
 # CS 1101 - Week 11 Videos - NumPy - Practice 09
+
+import matplotlib.pyplot as plt
+
+# Code 01
+# views = [534, 689, 258, 401, 724, 689, 350]
+# plt.plot(views)
+# plt.show()
+
+# Code 02
+views = [534, 689, 258, 401, 724, 689, 350]
+# days = [1, 2, 3, 4, 5, 6, 7]
+days = range(1, 8)
+# plt.plot(X,Y)
+plt.plot(days, views)
+plt.show()
