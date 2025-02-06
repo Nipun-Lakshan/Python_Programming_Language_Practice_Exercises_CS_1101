d000cc9d5d87343f8762a654503b4e916654889e
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-02-06 10:40:22 +0530

    Practice 68

---
+++ Practice68.py
@@ -0,0 +1,20 @@
+# CS 1101 - Week 11 - Exercise 04
+
+# Import the Numpy Library
+import numpy as np
+
+# One - Dimensional NumPy Array
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+# Two - Dimensional NumPy Array
+arr2d = np.array([[1, 2, 3], [4, 5, 6]]),
+print(arr2d)
+
+# Operations on One - Dimensional Array
+print(arr+4) # Work for individual elements. No need to run a loop.
+
+# Sum of Array
+print(np.sum(arr))
+
+# np.save('arr.npy', arr) -> Load to the Environment
