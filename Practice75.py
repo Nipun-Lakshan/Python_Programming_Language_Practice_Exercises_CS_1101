360e3de5a82d6c098946cd66b00a5ab6e1156bd8
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-02-13 10:22:58 +0530

    Practice 75

---
+++ Practice75.py
@@ -0,0 +1,59 @@
+# CS 1101 - Week 12 - Exercise 03
+
+# Importing External Libraries for Data Analysis Functions
+import numpy as np               # For Handling Mathematical Functions
+import matplotlib.pyplot as plt  # For Plotting Graphs
+import pandas as pd              # For Importing Files
+
+# Creating Data Frame Data Structure
+file_path = r"A:\03. CS 1101 - Temp\07. CSV Files\pendulum_L_T.csv"  # File Path as a String
+df = pd.read_csv(file_path)
+
+# Extracting Data as Columns from CSV File Following DataFrame Structure
+L1 = list(df['L'])
+T1 = list(df['T'])
+
+# Limiting Data to First 10 Points
+L = L1[:10]
+T = T1[:10]
+
+# Creating a RMSE List
+rmse_list = []
+
+for order in range(1, 6):
+    coefficient_fit = np.polyfit(L, T, deg=order)
+    f = np.poly1d(coefficient_fit)
+    XX = np.linspace(min(L), max(L), 1000)
+    Y1 = f(XX)
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.scatter(L, T, color='red', label='T')
+    plt.plot(XX, Y1, linestyle='--', color='blue', label='Order' + str(order))
+    plt.legend()
+    plt.title('L vs T')
+    plt.xlabel('L')
+    plt.ylabel('T')
+    plt.grid(True)
+
+    rmse = np.sqrt(np.mean((T - f(L))**2))
+    rmse_list.append(rmse)
+
+    residual = T - f(L)
+    plt.subplot(1,2,2)
+    plt.scatter(L, residual, color='green', label='T')
+    plt.legend()
+    plt.title('X vs Residuals For Order 0' + str(order))
+    plt.xlabel('L')
+    plt.ylabel('T')
+    plt.grid(True)
+    plt.show()
+
+print(rmse_list)
+plt.figure()
+plt.scatter([1,2,3,4,5], rmse_list, color='red')
+plt.plot([1,2,3,4,5], rmse_list, color='blue', linestyle='--')
+plt.title('Order vs RMSE')
+plt.xlabel('Order')
+plt.ylabel('RMSE')
+plt.grid(True)
+plt.show()
