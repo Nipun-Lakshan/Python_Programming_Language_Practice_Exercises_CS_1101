3863d1474bbd40a2b70e360e1cb06d360b9172b6
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-02-20 08:42:30 +0530

    Practice 78

---
+++ Practice78.py
@@ -0,0 +1,14 @@
+# CS 1101 - Week 13 - Exercise 01
+
+# Importing External Libraries for Data Analysis Functions
+import matplotlib.pyplot as plt  # For Plotting Graphs
+
+# X axis points
+x = [4, 5, 10, 4, 3, 11, 14, 6, 10, 12]
+
+# Y axis points
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+# Plot the Graph
+plt.plot(x, y, marker='o', linestyle=' ')
+plt.show()
